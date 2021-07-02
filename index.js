1a33968d7d2d1c6dd3ce8f85d139aa60e7b01f9c
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-07-01 19:06:47 -0500

    :art: [Add] express, nodemon

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 'use strict'
 
+//GraphQL
 const { graphql, buildSchema } = require('graphql')
+// Express
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+
+const app = express() // create the express system server
+const port = process.env.port || 3000 // Port for using our server
 
 // We define the schema
 const schema = buildSchema(`
@@ -19,8 +26,16 @@ const resolvers = {
   },
 }
 
-// Execute the query Hello
-// This returns a promise
-graphql(schema, '{goodbye}', resolvers).then((data) => {
-  console.log(data)
+// The first parameter is a root route into our server
+app.use(
+  '/api',
+  graphqlHTTP({
+    schema: schema, // Schema to execute
+    rootValue: resolvers, // Are the resolvers to execute?
+    graphiql: true, //The development environment
+  })
+)
+// we start the listener of the requires
+app.listen(port, () => {
+  console.log(`server is listening at port http://localhost:${port}/api`)
 })
